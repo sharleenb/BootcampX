95ae3e0db5b63ff8bebf6edd8bd92db9cdeb1335
Sharleen Beniamin <sharleenb16@gmail.com>
2023-04-07 18:53:19 +0000

    create queries using multiple tables

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(assistance_requests.*) as total_assistances, teachers.name as name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name
+;
---
+++ 4_queries/7_avg_request_wait.sql
@@ -0,0 +1,3 @@
+SELECT AVG(started_at-created_at) as average_wait_time
+FROM assistance_requests
+;
